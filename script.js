9b1f928210e756b567760a5cf7f3d01a44fe9bef
Bahadir Ungor <bahadirungor@gmail.com>
2022-01-16 19:51:36 +0300

    created required divs

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i<15 ;i++) {
+    const div = document.createElement("div");
+    container.appendChild(div);
+    div.classList.add("box")
+    for (let j = 0; j < 15; j++) {
+        const divv = document.createElement("div");
+        div.appendChild(divv);
+        divv.classList.add("box")
+    }
+}
