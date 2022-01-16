afbcf4bdcc5783dc7ca95025518568123cd07754
Bahadir Ungor <bahadirungor@gmail.com>
2022-01-16 20:09:23 +0300

    Made all the divs responsive

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ for (let i = 0; i<15 ;i++) {
     const div = document.createElement("div");
     container.appendChild(div);
     div.classList.add("box")
+    div.classList.add("row")
     for (let j = 0; j < 15; j++) {
         const divv = document.createElement("div");
         div.appendChild(divv);
